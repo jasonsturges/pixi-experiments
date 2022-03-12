2b7baf0c53d3ac374c720501a65bc96a805ac7fc
Jason Sturges <jason@jasonsturges.com>
2022-03-12 15:33:43 -0600

    Rounded rect stories

---
+++ stories/graphics/RoundedRect.stories.js
@@ -0,0 +1,42 @@
+import * as PIXI from "pixi.js";
+import "@pixi/graphics-extras";
+import { canvas, viewport } from "../Scene";
+import { parseColor } from "../../src/utils/ColorUtils";
+
+export default {
+  title: "Graphics/Shapes",
+  argTypes: {
+    stroke: { control: "number" },
+    color: { control: "color" },
+    fill: { control: "color" },
+    width: { control: "number" },
+    height: { control: "number" },
+    radius: { control: "number" },
+  },
+};
+
+export const RoundedRect = ({ stroke, color, fill, width, height, radius }) => {
+  const graphics = new PIXI.Graphics();
+  graphics.lineStyle(stroke, parseColor(color));
+  graphics.beginFill(parseColor(fill));
+
+  graphics.drawRoundedRect(
+    viewport.screenWidth / 2 - width / 2,
+    viewport.screenHeight / 2 - height / 2,
+    width,
+    height,
+    radius
+  );
+
+  viewport.addChild(graphics);
+
+  return canvas;
+};
+RoundedRect.args = {
+  stroke: 2,
+  color: "#cfefff",
+  fill: "#036191",
+  width: 100,
+  height: 100,
+  radius: 15,
+};
