febb7df9a936ed07e30e391334f5d9613b34ae60
Jason Sturges <jason@jasonsturges.com>
2022-03-12 15:16:01 -0600

    Star stories

--- stories/drawing/Star.stories.js
+++ stories/drawing/Star.stories.js
@@ -1,10 +1,11 @@
 import * as PIXI from "pixi.js";
+import "@pixi/graphics-extras";
 import { canvas, viewport } from "../Scene";
 import { drawStar } from "../../src/components/Star";
 import { parseColor } from "../../src/utils/ColorUtils";
 
 export default {
-  title: "Drawing/Shapes",
+  title: "Drawing/Shapes/Star",
   argTypes: {
     stroke: { control: "number" },
     color: { control: "color" },
@@ -44,6 +45,42 @@ export const Star = ({
   return canvas;
 };
 Star.args = {
+  stroke: 2,
+  color: "#cfefff",
+  fill: "#036191",
+  points: 5,
+  innerRadius: 25,
+  outerRadius: 50,
+  angle: 0,
+};
+
+export const StarGraphicsExtras = ({
+  stroke,
+  color,
+  fill,
+  points,
+  innerRadius,
+  outerRadius,
+  angle,
+}) => {
+  const graphics = new PIXI.Graphics();
+  graphics.lineStyle(stroke, parseColor(color));
+  graphics.beginFill(parseColor(fill));
+
+  graphics.drawStar(
+    viewport.screenWidth / 2,
+    viewport.screenHeight / 2,
+    points,
+    outerRadius,
+    innerRadius,
+    angle
+  );
+
+  viewport.addChild(graphics);
+
+  return canvas;
+};
+StarGraphicsExtras.args = {
   stroke: 2,
   color: "#cfefff",
   fill: "#036191",
