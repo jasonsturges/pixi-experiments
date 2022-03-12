e8665dc3f291d9ba94382b8e1125c8d870742c31
Jason Sturges <jason@jasonsturges.com>
2022-03-12 14:50:06 -0600

    Polygon stories

--- stories/drawing/Polygon.stories.js
+++ stories/drawing/Polygon.stories.js
@@ -1,10 +1,10 @@
 import * as PIXI from "pixi.js";
 import { canvas, viewport } from "../Scene";
-import { parseColor } from "../../src/utils/ColorUtils";
 import { drawPolygon } from "../../src/components/Polygon";
+import { parseColor } from "../../src/utils/ColorUtils";
 
 export default {
-  title: "Example/Polygon",
+  title: "Drawing/Shapes/Polygon",
   argTypes: {
     stroke: { control: "number" },
     color: { control: "color" },
@@ -13,6 +13,32 @@ export default {
     radius: { control: "number" },
     angle: { control: "number" },
   },
+};
+
+export const Polygon = ({ stroke, color, fill, sides, radius, angle }) => {
+  const graphics = new PIXI.Graphics();
+  graphics.lineStyle(stroke, parseColor(color));
+  graphics.beginFill(parseColor(fill));
+  drawPolygon(
+    graphics,
+    viewport.screenWidth / 2,
+    viewport.screenHeight / 2,
+    sides,
+    radius,
+    angle
+  );
+
+  viewport.addChild(graphics);
+
+  return canvas;
+};
+Polygon.args = {
+  stroke: 2,
+  color: "#cfefff",
+  fill: "#036191",
+  sides: 10,
+  radius: 50,
+  angle: 0,
 };
 
 const Template = ({ stroke, color, fill, sides, radius, angle }) => {
@@ -69,6 +95,26 @@ Hexagon.args = {
   color: "#cfefff",
   fill: "#036191",
   sides: 6,
+  radius: 50,
+  angle: 0,
+};
+
+export const Heptagon = Template.bind({});
+Heptagon.args = {
+  stroke: 2,
+  color: "#cfefff",
+  fill: "#036191",
+  sides: 7,
+  radius: 50,
+  angle: 0,
+};
+
+export const Octagon = Template.bind({});
+Octagon.args = {
+  stroke: 2,
+  color: "#cfefff",
+  fill: "#036191",
+  sides: 8,
   radius: 50,
   angle: 0,
 };
