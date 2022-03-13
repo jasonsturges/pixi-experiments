240a4372e2bad9a2c07050b8591c84dea819a751
Jason Sturges <jason@jasonsturges.com>
2022-03-12 23:31:13 -0500

    YIQ color contrast

--- src/utils/ColorUtils.ts
+++ src/utils/ColorUtils.ts
@@ -40,3 +40,17 @@ export function parseColor(value: number | string | undefined): number {
   // Failover
   return 0x0;
 }
+
+/**
+ * YIQ Color contrast calculation
+ */
+export function contrastYiq(color: number): number {
+  if (!color || !Number.isInteger(color)) return 0xffffff;
+
+  const r = (color >>> 16) & 0xff;
+  const g = (color >>> 8) & 0xff;
+  const b = color & 0xff;
+  const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+
+  return yiq >= 128 ? 0x0 : 0xffffff;
+}
