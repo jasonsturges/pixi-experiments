61304d9076e3edbf731b4163414b1cc484e7d930
Jason Sturges <jason@jasonsturges.com>
2022-03-21 15:00:12 -0500

    Anchor

---
+++ src/display/Anchor.ts
@@ -0,0 +1,56 @@
+import { AbstractControl } from "./AbstractControl";
+import { Graphics, ILineStyleOptions, Rectangle, Text } from "pixi.js";
+
+export class Anchor extends AbstractControl {
+  protected _graphics?: Graphics | undefined;
+  protected readonly _text?: string | undefined;
+  protected readonly _textField?: Text | undefined;
+
+  constructor() {
+    super();
+
+    this._graphics = new Graphics();
+    this._graphics.interactive = true;
+    this._graphics.lineStyle(<ILineStyleOptions>{
+      color: 0xffff00,
+      width: 1,
+    });
+    this._graphics.beginFill(0xffff00, 0.25);
+    this._graphics.drawCircle(0, 0, 4);
+    this._graphics.endFill();
+    this._graphics.hitArea = new Rectangle(-8, -8, 16, 16);
+    this.addChild(this._graphics);
+
+    this._textField = new Text("", {
+      fontSize: 6,
+      fill: 0x00ff33,
+      align: "center",
+    });
+    this._textField.resolution = 8;
+    this._textField.anchor.set(0.5);
+    this._textField.pivot.set(0, 10);
+    this.addChild(this._textField);
+  }
+
+  /**
+   * Invalidation pipeline
+   */
+  protected override validate() {
+    super.validate();
+
+    if (this._textField) this._textField.text = this._text ?? "";
+  }
+
+  /**
+   * Text
+   */
+  get text() {
+    return this._text;
+  }
+
+  set text(value) {
+    if (this._text === value) return;
+    this._text = value;
+    this.invalidate();
+  }
+}
